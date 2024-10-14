1cf7c2a93eeca7470f3eef3e29226efffced906c
Circe_Dellop <circe_dellop@outlook.com>
2024-10-14 11:34:41 -0600

    feat: Actualizacion de temas vistos hasta el 13102024 sin contar los temas de los dias 10 y 07

---
+++ js09NPM/js09-npm/script.js
@@ -0,0 +1,8 @@
+import { getUsers, getUsersAxios } from "./src/components/getUsers.js";
+
+
+getUsers("https://reqres.in/api/users?page=1");
+getUsersAxios("https://reqres.in/api/users?page=1");
+
+// getUsers("https://reqres.in/api/users?page=2");
+
---
+++ js09NPM/js09-vite/src/components/card/truncarTitulo.js
@@ -0,0 +1,19 @@
+
+const truncarTitulo = (titulo, maxCaracteres) => {
+  let tituloRecortado;
+  const decorador = "...";
+
+  if (titulo.length > maxCaracteres) {
+    tituloRecortado = titulo.slice(0, maxCaracteres - decorador.length);
+    // Born to Make You Happy
+    tituloRecortado += decorador;
+  } else {
+    tituloRecortado = titulo;
+  }
+
+  return tituloRecortado;
+};
+
+console.log( truncarTitulo("Ya nos vamos al primer descanso", 10 ) );
+// export default truncarTitulo;
+export { truncarTitulo };
